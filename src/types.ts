e76f360fcde09317c3287eb31005fadf27005571
shai sarfaty <shai.sarfaty@gmail.com>
2025-12-29 11:40:42 +0200

    commit new version v1.6.0

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-export interface FileItem {
-    name: string;
-    uri: vscode.Uri;
-    type: vscode.FileType;
-}
-
-export type FileSortOrder = 'default' | 'mixed' | 'filesFirst' | 'type' | 'modified';
